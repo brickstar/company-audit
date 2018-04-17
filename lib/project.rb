f89e2a0aaa2224ad3c91e18e00d9979f7dd4ff60
brickstar <brickstar@gmail.com>
2018-04-17 10:26:03 -0600

    fixes project class/tests

--- lib/project.rb
+++ lib/project.rb
@@ -5,7 +5,7 @@ class Project
               :end_date
 
   def initialize(project_id, name, start_date, end_date)
-    @project_id = project_id
+    @project_id = project_id.to_i
     @name       = name
     @start_date = Date.parse(start_date)
     @end_date   = Date.parse(end_date)
--- test/projects_test.rb
+++ test/projects_test.rb
@@ -4,11 +4,7 @@ require 'pry'
 
 class ProjectsTest < Minitest::Test
   def setup
-    @p = Project.new({  :project_id => '123',
-                        :name => 'Widget Maker',
-                        :start_date => '2015-01-01',
-                        :end_date => '2018-01-01',
-                      })
+    @p = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
   end
 
   def test_instantiation
@@ -16,7 +12,7 @@ class ProjectsTest < Minitest::Test
   end
 
   def test_attributes
-    assert_equal '123', @p.project_id
+    assert_equal 123, @p.project_id
     assert_equal Integer, @p.project_id.class
     assert_equal 'Widget Maker', @p.name
     assert_instance_of Date, @p.start_date
