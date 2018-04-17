23f25bf09fbd4be588c668e954b7a9b835da5cd7
brickstar <brickstar@gmail.com>
2018-04-17 09:10:50 -0600

    initial commit

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,9 +2,25 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+  def setup
+    @e = Employee.new({ :id => 5,
+                        :name => 'Sally Jackson',
+                        :role => 'Engineer',
+                        :start_date => '2015-01-01',
+                        :end_date => '2018-01-01'
+                       })
+  end
+
   def test_instantiation
+    assert_instance_of Employee, @e
   end
 
   def test_attributes
   end
 end
+# employee_id = '5'
+# name = 'Sally Jackson'
+# role = 'Engineer'
+# start_date = '2015-01-01'
+# end_date = '2018-01-01'
+# employee = Employee.new(employee_id, name, role, start_date, end_date)
